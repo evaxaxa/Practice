6e6b269aac811ac9f7af89140e233a34ef522761
Eva Ananta Xaxa <evaanantaxaxa@Evas-MacBook-Pro.local>
2021-08-31 13:12:34 +0530

    added files!!!!

--- src/Count.java
+++ src/Count.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Count {
-}
+import java.util.*;
+public class Count{
+    public static void main(String args[]){
+
+        String str = "Malayalam";
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();//Creating HashMap
+      // map.put('M',1);  //Put elements in Map
+       /* map.put(2,"Apple");
+        map.put(3,"Banana");
+        map.put(4,"Grapes");*/
+        for(int i=0;i<str.length();i++)
+        {
+            System.out.println(map.get(str.charAt(i)));
+            int value =  map.get(str.charAt(i));
+
+           if(map.containsKey(str.charAt(i))==true)
+            {
+
+              if(value==0)
+              {
+                  value=1;
+              }
+              else value++;
+
+                map.put(str.charAt(i),value);
+
+            }
+           else{
+               map.put(str.charAt(i),value);
+           }
+
+        }
+
+
+        System.out.println("Iterating Hashmap...");
+        for(Map.Entry m : map.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+    }
+}  
--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MissingNumber {
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    int MissNum(int a[])
+    {
+        Arrays.sort(a);
+        for(int i =0;i<a.length-1;i++)
+        {
+            if(a[i]+1!=a[i+1])
+            {
+                int k= a[i]+1;
+                System.out.println("missing num ="+k);
+            }
+            /*else {
+                int k= a[i]+1;
+                System.out.println("missing num ="+k);
+            }*/
+        }
+
+        return 0;
+    }
+
+    public static void main(String args[])
+    {
+        MissingNumber g = new MissingNumber();
+        int a[]= {-3,-1,0,1, 2, 3, 5};
+        g.MissNum(a);
+    }
+
 }
